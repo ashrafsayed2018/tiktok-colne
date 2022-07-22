50e3a7906b0889b32f8a5083f274b5c6e62e70ab
ashrafsayed2018 <ashrafsayed19841111@gmail.com>
2022-07-23 01:56:32 +0300

    confirm video screen

--- lib/views/screens/add_video_screen.dart
+++ lib/views/screens/add_video_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:tiktok_clone/constants.dart';
@@ -9,8 +11,11 @@ class AddVideoScreen extends StatelessWidget {
   pickVideo(ImageSource src, BuildContext context) async {
     final video = await ImagePicker().pickVideo(source: src);
     if (video != null) {
-      Navigator.of(context)
-          .push(MaterialPageRoute(builder: (context) => const ConfirmScreen()));
+      Navigator.of(context).push(MaterialPageRoute(
+          builder: (context) => ConfirmScreen(
+                videoFile: File(video.path),
+                videoPath: video.path,
+              )));
     }
   }
 
--- lib/views/screens/confirm_screen.dart
+++ lib/views/screens/confirm_screen.dart
@@ -1,10 +1,98 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/views/widgets/text_input_field.dart';
+import 'package:video_player/video_player.dart';
 
-class ConfirmScreen extends StatelessWidget {
-  const ConfirmScreen({Key? key}) : super(key: key);
+class ConfirmScreen extends StatefulWidget {
+  final File? videoFile;
+  final String videoPath;
+  const ConfirmScreen({
+    Key? key,
+    required this.videoFile,
+    required this.videoPath,
+  }) : super(key: key);
+
+  @override
+  State<ConfirmScreen> createState() => _ConfirmScreenState();
+}
+
+class _ConfirmScreenState extends State<ConfirmScreen> {
+  late VideoPlayerController controller;
+  TextEditingController songController = TextEditingController();
+  TextEditingController captionController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    setState(() {
+      controller = VideoPlayerController.file(widget.videoFile!)
+        ..initialize().then((_) {
+          controller.setLooping(true);
+          controller.setVolume(1);
+          controller.play();
+        });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(children: [
+          const SizedBox(
+            height: 30,
+          ),
+          SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height * 0.6,
+            child: VideoPlayer(controller),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Column(
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  margin: const EdgeInsets.symmetric(horizontal: 10),
+                  child: TextInputField(
+                    keyboardType: TextInputType.text,
+                    controller: songController,
+                    labelText: "مقطع موسيقى",
+                    hintText: "مقطع موسيقى",
+                    icon: Icons.music_note,
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  margin: const EdgeInsets.symmetric(horizontal: 10),
+                  child: TextInputField(
+                    keyboardType: TextInputType.text,
+                    controller: captionController,
+                    labelText: "عنوان الفيديو",
+                    hintText: "عنوان الفيديو",
+                    icon: Icons.closed_caption,
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                ElevatedButton(
+                    onPressed: () {},
+                    child: const Text(
+                      "مشاركه",
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ))
+              ],
+            ),
+          )
+        ]),
+      ),
+    );
   }
 }
